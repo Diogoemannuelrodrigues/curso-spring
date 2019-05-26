3bc35a4d08e4f57ab0f0cf4057e8d7448c3222d5
diogoemannuel <diogoemannuel.rodrigues1@gmail.com>
2019-05-26 16:38:25 -0300

    adicionando um comentario

--- jrcosmeticos/src/main/java/com/jrcosmeticos/jrcosmeticos/JrcosmeticosApplication.java
+++ jrcosmeticos/src/main/java/com/jrcosmeticos/jrcosmeticos/JrcosmeticosApplication.java
@@ -8,6 +8,8 @@ public class JrcosmeticosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JrcosmeticosApplication.class, args);
+        
+        //comentario;
 	}
 
 }
